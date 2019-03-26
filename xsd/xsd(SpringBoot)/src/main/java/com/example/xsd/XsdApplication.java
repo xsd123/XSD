bbb647c77e5eeae35b3d1f027b766b942d1aec46
Your Name <you@example.com>
2019-03-26 09:58:57 +0800

    project commit

---
+++ xsd/xsd(SpringBoot)/src/main/java/com/example/xsd/XsdApplication.java
@@ -0,0 +1,15 @@
+package com.example.xsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class XsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XsdApplication.class, args);
+	}
+
+}
---
+++ xsd/xsd(SpringBoot)/src/main/java/com/shiro/admin/controller/AdminController.java
@@ -0,0 +1,4 @@
+//package com.shiro.admin.controller;
+//
+//public class AdminController {
+//}
